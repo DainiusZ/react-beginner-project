f1394b91f0e2d3017b5677425dbf8b91adcfa16e
Dainius <d.zandaravicius@gmail.com>
2019-01-18 23:41:44 +0200

    add fish form component and basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,41 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    //1.Make a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    //2.Add a new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3.Set new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,15 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
       <div className="inventory">
-        <h1>Inventory!!!</h1>
+        <h2>Inventory</h2>
+        <AddFishForm addFish={this.props.addFish} />
+        <button onClick={this.props.loadSample.Fishes}>
+          Load Sample Fishes
+        </button>
       </div>
     );
   }
